7b1fade3d7c2d3af58b3d4d7610aa48f16449e3c
Cuong Giang <thaicuong.giang@gmail.com>
2024-06-22 17:57:38 +0700

    fix migrations

--- stubs/default/db/migrate/20240101000000_create_users.rb
+++ stubs/default/db/migrate/20240101000000_create_users.rb
@@ -1,11 +1,13 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |table|
-      table.string :name
-      table.string :email
-      table.timestamp :email_verified_at, null: true
-      table.string :password_digest
-      table.string :remember_token, null: true
+      table.string :name, null: false
+      table.string :email, null: false
+      table.timestamp :email_verified_at
+      table.string :password_digest, null: false
+      table.string :remember_token
+      table.integer :current_team_id
+      table.string :profile_photo_path, limit: 2048
       table.timestamps
     end
   end
