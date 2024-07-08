10384d5375e1b0031a28c790b4289d0aee4c8f69
Cuong Giang <thaicuong.giang@gmail.com>
2024-07-08 10:02:14 +0700

    update react components for @headlessui/react v2

--- stubs/inertia-react-ts/app/javascript/Components/Dropdown.tsx
+++ stubs/inertia-react-ts/app/javascript/Components/Dropdown.tsx
@@ -1,4 +1,4 @@
-import { useState, createContext, useContext, Fragment, PropsWithChildren, Dispatch, SetStateAction } from 'react'
+import { useState, createContext, useContext, PropsWithChildren, Dispatch, SetStateAction } from 'react'
 import { Link, InertiaLinkProps } from '@inertiajs/react'
 import { Transition } from '@headlessui/react'
 
@@ -43,11 +43,7 @@ const Content = ({
   width = '48',
   contentClasses = 'py-1 bg-white dark:bg-gray-700',
   children,
-}: PropsWithChildren<{
-  align?: 'left' | 'right'
-  width?: '48'
-  contentClasses?: string
-}>) => {
+}: PropsWithChildren<{ align?: 'left' | 'right'; width?: '48'; contentClasses?: string }>) => {
   const { open, setOpen } = useContext(DropDownContext)
 
   let alignmentClasses = 'origin-top'
@@ -67,7 +63,6 @@ const Content = ({
   return (
     <>
       <Transition
-        as={Fragment}
         show={open}
         enter="transition ease-out duration-200"
         enterFrom="opacity-0 scale-95"
